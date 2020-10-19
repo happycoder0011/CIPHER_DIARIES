9b1b24716c23f4a14048bc1f4c2fbbb9cea46b48
LyndonFan <lyndon0808@gmail.com>
2020-10-19 01:06:30 +0100

    fix & describe sub cipher

--- Substitution_Cipher/Substitution_Cipher.py
+++ Substitution_Cipher/Substitution_Cipher.py
@@ -1,12 +1,12 @@
 # Python program to demonstrate 
 # Substitution Cipher 
-  
-  
-import string 
+
+import string
+import re
   
   
 # A list containing all characters 
-all_letters= string.ascii_letters 
+all_letters = string.ascii_letters
    
       
 """  
@@ -14,31 +14,38 @@ create a dictionary to store the substitution
 for the given alphabet in the plain text  
 based on the key 
 """
-   
+
+key = input("Enter a keyword: ")
+key = key.strip()
+ciph_alpha = ""
+for c in key:
+    if c in all_letters and not(c.lower() in ciph_alpha):
+        ciph_alpha += c.lower()
+for c in all_letters[:26]:
+    if not(c in ciph_alpha):
+        ciph_alpha += c
       
-dict1 = {} 
-key = 4
+dict1 = {}
    
-for i in range(len(all_letters)): 
-    dict1[all_letters[i]] = all_letters[(i+key)%len(all_letters)] 
-   
-   
-plain_txt = input("Enter your message: ")
-cipher_txt=[] 
+for i in range(26): 
+    dict1[all_letters[i]] = ciph_alpha[i]
+    dict1[all_letters[i+26]] = ciph_alpha[i].upper()
+
+print("Plaintext alphabet:   "+all_letters[26:])
+print("Ciphertext alphabet:  "+ciph_alpha.upper())
+
+plain_txt = input("Enter your message:   ")
+cipher_txt = ""
    
 # loop to generate ciphertext 
    
 for char in plain_txt: 
     if char in all_letters: 
-        temp = dict1[char] 
-        cipher_txt.append(temp) 
-    else: 
-        temp =char 
-        cipher_txt.append(temp) 
+        cipher_txt += dict1[char]
+    else:
+        cipher_txt += char
           
-cipher_txt= "".join(cipher_txt) 
-print("Cipher Text is: ",cipher_txt) 
-   
+print("Ciphertext:          ",cipher_txt) 
       
 """  
 create a dictionary to store the substitution 
@@ -48,19 +55,17 @@ text based on the key
    
       
 dict2 = {}      
-for i in range(len(all_letters)): 
-    dict2[all_letters[i]] = all_letters[(i-key)%(len(all_letters))] 
+for i in range(26): 
+    dict2[ciph_alpha[i]] = all_letters[i]
+    dict2[ciph_alpha[i].upper()] = all_letters[i+26]
        
 # loop to recover plain text 
-decrypt_txt = [] 
+decrypt_txt = ""
   
 for char in cipher_txt: 
     if char in all_letters: 
-        temp = dict2[char] 
-        decrypt_txt.append(temp) 
+        decrypt_txt += dict2[char]
     else: 
-        temp = char 
-        decrypt_txt.append(temp) 
+        decrypt_txt += char
           
-decrypt_txt = "".join(decrypt_txt) 
-print("Recovered plain text :", decrypt_txt)  
+print("Recovered plaintext :", decrypt_txt)  
