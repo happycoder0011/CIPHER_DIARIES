e750056844be3f652e4905492f5db9815e787b5a
appledora <nazia89@student.sust.edu>
2021-10-06 00:28:34 +0600

    added decryption to Caeser Cipher.py

--- Caesar_Cipher/Caesar_Cipher.py
+++ Caesar_Cipher/Caesar_Cipher.py
@@ -1,9 +1,45 @@
+import string
+
 #Encryption of Text
 '''
 Text : ATTACKATONCE
 Shift: 4
 Cipher: EXXEGOEXSRGI
 '''
+#Decryptiopn of Cipher
+'''
+Cipher: krclxrwrbxwnxocqnlxxunbcrwencrxwbrwanlnwccrvnb.
+Shift: 9
+Text:: bitcoinisoneofthecoolestinvetionsinrecenttimes
+'''
+
+alpha = string.ascii_letters
+
+def ceaser_decrypt_without_key(target):
+    print("DECRYPTING....")
+    key = 0
+    while key <= 26:
+        temp = ""
+        for char in target:
+            if char.isalpha():
+                temp += alpha[(alpha.index(char)-int(key)) % 26]
+            else:
+                temp += char
+        print(temp + " ........... key: ", key)
+        key = key + 1
+
+
+def ceaser_decrypt(target, shift):
+    print("DECRYPTING....")
+    temp = ""
+    temp = ""
+    for char in target:
+        if char.isalpha():
+            temp += alpha[(alpha.index(char)-int(shift)) % 26]
+        else:
+            temp += char
+    print(temp)
+
 
 def encrypt(text,s): 
     result = "" 
