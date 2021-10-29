a177a44f186460a148e12a0c6eab3761f850bef9
Darahaas Yajamanyam <darahaas15@gmail.com>
2021-10-29 05:36:20 +0530

    Update Substitution_Cipher.c

--- Substitution_Cipher/Substitution_Cipher.c
+++ Substitution_Cipher/Substitution_Cipher.c
@@ -1,132 +1,53 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#include <conio.h>
- 
-int main() {
- 
-	char *message,*emessage,*dmessage;
- 
-	int i,j=0,k,key,temp;
- 
-	clrscr();
- 
-	printf("\nEnter the key\n");
- 
-	scanf("%d",&key);
- 
-	key=key%26;
- 
-	printf("\nEnter message\n");
- 
-	fflush(stdin);
- 
-	gets(message);
- 
-	for (i=0;message[i]!=NULL;i++)
- 
-	  message[i]=tolower(message[i]);
- 
-	for (i=0;message[i]!=NULL;i++) {
- 
-		//printf("%c ",message[i]);
- 
-		if(message[i]==' ')
- 
-		    emessage[j++]=message[i]; else {
- 
-			if(message[i]>=48 && message[i]<=57) {
- 
-				temp=message[i]+key;
- 
-				if(temp>57)
- 
-				         emessage[j++]=48+(temp-58); else
- 
-				         emessage[j++]=temp;
- 
-			} else {
- 
-				if(message[i]>=97 && message[i]<=123) {
- 
-					temp=message[i]+key;
- 
-					if(temp>122)
- 
-					             emessage[j++]=97+(temp-123); else
- 
-					             emessage[j++]=temp;
- 
-				} else
- 
-				              emessage[j++]=message[i];
- 
-			}
- 
-			// printf("%c ",emessage[j]);
- 
-		}
- 
-	}
- 
-	emessage[j]='\0';
- 
-	printf("\n\n\nEncrypted message is\n\n");
- 
-	for (i=0;emessage[i]!=NULL;i++)
- 
-	  printf("%c",emessage[i]);
- 
-	//  printf("\n end");
- 
-	for (i=0,j=0;emessage[i]!=NULL;i++) {
- 
-		if(emessage[i]==' ')
- 
-		     dmessage[j++]=emessage[i]; else {
- 
-			if(emessage[i]>=48 && emessage[i]<=57) {
- 
-				temp=emessage[i]-key;
- 
-				if(temp<48)
- 
-				         dmessage[j++]=58-(48-temp); else
- 
-				         dmessage[j++]=temp;
- 
-			} else {
- 
-				if(emessage[i]>=97 && emessage[i]<=123) {
- 
-					temp=emessage[i]-key;
- 
-					if(temp<97)
- 
-					           dmessage[j++]=123-(97-temp); else
- 
-					           dmessage[j++]=temp;
- 
-				} else
- 
-				          dmessage[j++]=emessage[i];
- 
-			}
- 
-		}
- 
-	}
- 
-	dmessage[j]='\0';
- 
-	printf("\n\n\nRetrieved message is\n\n");
- 
-	for (i=0;dmessage[i]!=NULL;i++)
- 
-	  printf("%c",dmessage[i]);
- 
-	getch();
- 
-	return(0);
- 
+int find_index(char code[],char char_to_find){
+  for(int i = 0 ; i < 26; i ++){
+    if(code[i] == char_to_find){
+      return i;
+    }
+  }
+  return -1;
+}
+
+char* encrypt(char *message,char code[]){
+  int length = strlen(message);
+  char *encrypted_message = (char *) malloc(sizeof(char)*length);
+
+  for(int i = 0; i < length; i++){
+    int encryption_index = tolower(message[i]) - 'a';
+    if(encryption_index >= 0 && encryption_index < 26){
+      encrypted_message[i] = code[encryption_index];
+    }else{
+      encrypted_message[i] = message[i];
+    }
+  }
+  return encrypted_message;
+}
+
+char *decrypt(char *message,char code[]){
+  int length = strlen(message);
+  char *decrypted_message = (char *) malloc(sizeof(char)*length);
+  for(int i = 0; i <length; i++){
+    int decryption_index = tolower(message[i]) - 'a';
+    if(decryption_index >= 0 && decryption_index < 26){
+      int code_index = find_index(code,tolower(message[i]));
+      decrypted_message[i] = 'a' + code_index;
+    }else{
+      decrypted_message[i] = message[i];
+    }
+  }
+  return decrypted_message;
+}
+
+int main(){
+  char *message = "test message";
+  char code[26] = {'b','a','c','d','s','f','g','h','i','j','k','l','m','n','o','p','q','r','e','t','u','v','w','x','y','z'};
+
+  char *encrypted_message = encrypt(message,code);
+  printf("Original Message: %s\nEncrypted Message: %s\n",message,encrypted_message);
+  char *decrypted_message = decrypt(encrypted_message,code);
+  printf("Decrypted Message: %s\n",decrypted_message);
 }
